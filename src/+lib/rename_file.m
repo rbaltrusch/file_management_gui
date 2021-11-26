b4dd1bf86c896db29ed6669ce0577339c48ec6a1
Richard Baltrusch <richard@baltrusch.net>
2021-11-26 22:25:43 +0100

    Add help documentation to rename_file.m

--- src/+lib/rename_file.m
+++ src/+lib/rename_file.m
@@ -1,3 +1,15 @@
+%Given an input filepath, renames that file and returns the new file name.
+%
+%Optional parameters:
+%   find    (char)      string to be matched by a replace function
+%   replace (char)      replacement for matched string
+%   prefix  (char)      string to be added to start of file name
+%   suffix  (char)      string to be added to end of file name
+%   regexp  (logical)   turns on regexprep instead of strrep
+%
+%Author: Richard Baltrusch
+%Date: 26/11/2021
+
 function new_filename = rename_file(filepath, varargin)
     args = parse_inputs(varargin{:});
     [~, filename, extension] = fileparts(filepath);
